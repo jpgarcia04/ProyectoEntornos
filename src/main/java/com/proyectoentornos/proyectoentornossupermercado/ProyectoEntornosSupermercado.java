6e7a61dc0be775ab1e8bffbf15834138035ea112
unknown <juanpagm04@gmail.com>
2026-05-19 13:11:31 +0200

    Subir proyecto NetBeans

---
+++ src/main/java/com/proyectoentornos/proyectoentornossupermercado/ProyectoEntornosSupermercado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyectoentornos.proyectoentornossupermercado;
+
+/**
+ *
+ * @author MEDAC
+ */
+public class ProyectoEntornosSupermercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
